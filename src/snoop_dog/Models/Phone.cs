357fbe17171d30f711ad715ae56458f7afa8c73d
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2016-12-20 18:04:37 +0300

    ef customized

---
+++ src/snoop_dog/Models/Phone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace snoop_dog.Models
+{
+    public class Phone
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public int Price { get; set; }
+    }
+}
