aa5d494aa79b46862a83fe8b22a46345622e7a6f
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2016-12-21 14:50:11 +0300

    connect with db

---
+++ src/snoop_dog/Migrations/20161220220955_smoke.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace snoop_dog.Migrations
+{
+    public partial class smoke : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- src/snoop_dog/Startup.cs
+++ src/snoop_dog/Startup.cs
@@ -7,15 +7,33 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using snoop_dog.Models;
+using Microsoft.EntityFrameworkCore; // пространство имен EntityFramework
+using Microsoft.Extensions.Configuration;
 
 namespace snoop_dog
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+        public IConfigurationRoot Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            string connection = Configuration.GetConnectionString("14ServerConnection");
+            services.AddDbContext<MobileContext>(options =>
+                options.UseSqlServer(connection));
             services.AddMvc();
         }
+
         
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
